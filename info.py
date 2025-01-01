5c633e46ba135edd92606e804ce292124134e389
TOICTONY <114583077+TOICTONY@users.noreply.github.com>
2025-01-01 20:00:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -166,7 +166,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://rockerz2z-e009dd832b03.herokuapp.com/")
+URL = environ.get("URL", "https://rockerz2zxbot-f515bee25111.herokuapp.com/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
