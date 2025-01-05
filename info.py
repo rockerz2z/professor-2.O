366854fbab21b02f951da778328c276dac3fb506
Rocker2k <119720415+rockerz2z@users.noreply.github.com>
2025-01-05 13:15:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://r2kfilter:r2kfilter @cluster0.utqrp.mongodb.net/?retryWrites=true&w=majority")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://r2kfilter2:r2kfilter2@cluster0.eiz1m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
 DATABASE_NAME = environ.get('DATABASE_NAME', "RockersClob")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
